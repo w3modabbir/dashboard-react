6b5532817a9f6531f9198d2d93a49211cb59c3de
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-02-26 19:48:43 +0600

    menu bar done

--- src/componants/sidebar/menu/Menu.jsx
+++ src/componants/sidebar/menu/Menu.jsx
@@ -2,32 +2,31 @@ import React from 'react'
 import './menu.scss'
 import { Link } from 'react-router-dom'
 import { MdHome } from "react-icons/md";
+import { menu } from '../../../data';
+import LoginImg from '../../../assets/svg/home.svg'
+import Image from '../../Image';
 
 const Menu = () => {
   return (
     <div className='menu_main'>
-      <div className="menu_item">
-        <h4 className='title'>Mian</h4>
-          <Link to="/" className='list_item'>
-            <MdHome />
-            <h3 className='hometitle'>Home</h3>
-          </Link>
-          <Link to="/" className='list_item'>
-            <MdHome />
-            <h3 className='hometitle'>Profile</h3>
-          </Link>
-      </div>
-      <div className="menu_item">
-        <h4 className='title'>Mian</h4>
-        <Link to="/" className='list_item'>
-            <MdHome />
-            <h3 className='hometitle'>Home</h3>
-          </Link>
-          <Link to="/" className='list_item'>
-            <MdHome />
-            <h3 className='hometitle'>Profile</h3>
-          </Link>
-      </div>
+      {menu.map((item)=>(
+        <div key={item.id} className="menu_item">
+          <h4 className='title'>{item.title}</h4>
+          {item.listItems.map((listItem)=>(
+            <Link to="/" className='list_item' key={listItem.id}>
+                {/* <Image  source={listItem.icon} alt="not found"/> */}
+              <h3 className='hometitle'>{listItem.title}</h3>
+            </Link>
+          ))
+
+          }
+         
+        </div>
+      ))
+      
+      }
+      
+     
     </div>
   )
 }
