8dcc06e181d29f91073ce066c3ef0a674abdd16d
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-02-26 16:44:47 +0600

    menu part

--- src/componants/sidebar/menu/Menu.jsx
+++ src/componants/sidebar/menu/Menu.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import './menu.scss'
+import { Link } from 'react-router-dom'
+import { MdHome } from "react-icons/md";
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className='menu_main'>
+      <div className="menu_item">
+        <h4 className='title'>Mian</h4>
+          <Link to="/" className='list_item'>
+            <MdHome />
+            <h3 className='hometitle'>Home</h3>
+          </Link>
+          <Link to="/" className='list_item'>
+            <MdHome />
+            <h3 className='hometitle'>Profile</h3>
+          </Link>
+      </div>
+      <div className="menu_item">
+        <h4 className='title'>Mian</h4>
+        <Link to="/" className='list_item'>
+            <MdHome />
+            <h3 className='hometitle'>Home</h3>
+          </Link>
+          <Link to="/" className='list_item'>
+            <MdHome />
+            <h3 className='hometitle'>Profile</h3>
+          </Link>
+      </div>
+    </div>
   )
 }
 
