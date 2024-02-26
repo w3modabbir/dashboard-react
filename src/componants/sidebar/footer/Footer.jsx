1d606d62663245e44514faec69aa4f6000a654ea
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-02-26 15:51:34 +0600

    footer  part done

--- src/componants/sidebar/footer/Footer.jsx
+++ src/componants/sidebar/footer/Footer.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Image from '../../Image'
+import LogoImg from '../../../assets/images/logo.png'
+import './footer.scss'
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+      <div>
+        <Image source={LogoImg} alt="not found"/>
+      </div>
+      <div>
+        <p>Copyright © 2024 <span>Modabbir</span>. Designed with  by <span>Modabbir</span> All rights reserved</p>
+      </div>
+    </div>
   )
 }
 
